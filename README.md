36d64b2d9e119c23925f6ada964fe6c60441d1fa
Brendan Molloy <brendan@bbqsrc.net>
2022-05-07 13:47:12 +0200

    Add more flags

---
+++ README.md
@@ -0,0 +1,32 @@
+# xml-pretty
+
+Make your XML pretty. A frontend to the (`xmlem`)[https://github.com/bbqsrc/xmlem] crate.
+
+### Features
+
+- It pretty prints your XML
+- Outputs to file, terminal or replaces original file
+- Handle entities properly and can enforce hex entities
+- Customisable indentation width and maximum file width
+- Ought not eat your comments even if they're outside the root element
+
+## Installation
+
+```
+cargo install --git https://github.com/bbqsrc/xml-pretty
+```
+
+## Usage
+
+Use `xml-pretty --help` to see the flags.
+
+Simplest invocation is `xml-pretty <file>`.
+
+## License
+
+This project is licensed under either of
+
+ * Apache License, Version 2.0 ([LICENSE-APACHE](LICENSE-APACHE) or http://www.apache.org/licenses/LICENSE-2.0)
+ * MIT license ([LICENSE-MIT](LICENSE-MIT) or http://opensource.org/licenses/MIT)
+
+at your option.
--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,15 @@ use std::{
 
 use anyhow::Context;
 use gumdrop::Options;
-use xmlem::Document;
+use xmlem::{display, Document};
 
 #[derive(Debug, Options)]
 struct Args {
     #[options(help = "display help information")]
     help: bool,
+
+    #[options(free, help = "path to XML document")]
+    xml_document_path: Option<PathBuf>,
 
     #[options(help = "output to file")]
     output_path: Option<PathBuf>,
@@ -18,8 +21,18 @@ struct Args {
     #[options(short = "r", long = "replace", help = "replace input file with output")]
     is_replace: bool,
 
-    #[options(free, help = "path to XML document")]
-    xml_document_path: Option<PathBuf>,
+    #[options(help = "number of spaces to indent (default: 2)")]
+    indent: Option<usize>,
+
+    #[options(short = "l", help = "max line length (default: 120)")]
+    max_line_length: Option<usize>,
+
+    #[options(
+        short = "H",
+        long = "hex-entities",
+        help = "Use hex entity encoding (e.g. &#xNNNN;) for all entities"
+    )]
+    uses_hex_entities: bool,
 }
 
 fn main() -> anyhow::Result<()> {
@@ -41,8 +54,13 @@ fn main() -> anyhow::Result<()> {
         None
     };
 
-    let text = prettify(&input_path)
-        .with_context(|| format!("Failed to prettify '{}'", input_path.display()))?;
+    let text = prettify(
+        &input_path,
+        args.indent,
+        args.max_line_length,
+        args.uses_hex_entities,
+    )
+    .with_context(|| format!("Failed to prettify '{}'", input_path.display()))?;
 
     if let Some(path) = output_path {
         write(&path, text).with_context(|| format!("Failed to write to '{}'", path.display()))?;
@@ -53,8 +71,22 @@ fn main() -> anyhow::Result<()> {
     Ok(())
 }
 
-fn prettify(path: &Path) -> anyhow::Result<String> {
+fn prettify(
+    path: &Path,
+    indent: Option<usize>,
+    max_line_length: Option<usize>,
+    uses_hex_entities: bool,
+) -> anyhow::Result<String> {
     let file = File::open(path)?;
     let doc = Document::from_file(file)?;
-    Ok(doc.to_string_pretty())
+    Ok(doc.to_string_pretty_with_config(&display::Config {
+        is_pretty: true,
+        indent: indent.unwrap_or(2),
+        max_line_length: max_line_length.unwrap_or(120),
+        entity_mode: if uses_hex_entities {
+            display::EntityMode::Hex
+        } else {
+            display::EntityMode::Standard
+        },
+    }))
 }
